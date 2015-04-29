44f386a185687bf6c248d759b4a5ccd53e651273
hien <hienhyn@gmail.com>
2015-04-29 11:15:36 +0700

    customize UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,32 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # before_action :authenticate_user!
   protect_from_forgery with: :exception
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+  end
+    # custom 404
+  # if Rails.env.production?
+  #   unless Rails.application.config.consider_all_requests_local
+  #     rescue_from Exception, with: :render_500
+  #     rescue_from ActionController::RoutingError, with: :render_404
+  #     rescue_from ActionController::UnknownController, with: :render_404
+  #     rescue_from ActionController::UnknownAction, with: :render_404
+  #     rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  #   end
+  # end
+  # def render_404(exception)
+  #   @not_found_path = exception.message
+  #   respond_to do |format|
+  #     format.html { render template: 'errors/not_found', layout: 'layouts/application', status: 404 }
+  #     format.all { render nothing: true, status: 404 }
+  #   end
+  # end
+
+  # def render_500(exception)
+  #   logger.info exception.backtrace.join("\n")
+  #   respond_to do |format|
+  #     format.html { render template: 'errors/internal_server_error', layout: 'layouts/application', status: 500 }
+  #     format.all { render nothing: true, status: 500}
+  #   end
+  # end
 end
